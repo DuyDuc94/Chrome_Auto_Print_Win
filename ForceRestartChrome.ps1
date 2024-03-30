799d5cd47d8ffb01edfa71a83866aaede7ffc41d
DuyDuc94 <duyduc.luonghuu@gmail.com>
2024-03-30 15:15:11 +0700

    fix: close all existing Chrome window

--- ForceRestartChrome.ps1
+++ ForceRestartChrome.ps1
@@ -1,3 +1,4 @@
-Get-Process "chrome" | ForEach-Object { $_.CloseMainWindow() | Out-Null }
+# Get-Process "chrome" | ForEach-Object { $_.CloseMainWindow() | Out-Null }
+Get-Process "chrome" | Stop-Process
 $chromeArgs = "--restore-last-session", "--kiosk-printing", "--disable-popup-blocking", "--use-system-default-print"
 Start-Process "chrome" -ArgumentList $chromeArgs
