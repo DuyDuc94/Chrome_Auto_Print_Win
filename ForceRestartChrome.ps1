add5855e7dd97b18ef9248282f19b477f0afe950
DuyDuc94 <duyduc.luonghuu@gmail.com>
2024-03-30 17:21:56 +0700

    fix: force to close all current chrome window

--- ForceRestartChrome.ps1
+++ ForceRestartChrome.ps1
@@ -1,4 +1,7 @@
-# Get-Process "chrome" | ForEach-Object { $_.CloseMainWindow() | Out-Null } # this shutdow only current selected instances of Chrome with behavior same as user click on Close button
-Get-Process "chrome" | Stop-Process # this force to shutdown all process of Chrome as behavior of crash
+do {
+    Get-Process chrome -ErrorAction SilentlyContinue | ForEach-Object { $_.CloseMainWindow() | Out-Null } # this shutdow only current selected instances of Chrome with behavior same as user click on Close button
+    # Start-Sleep -Milliseconds 100  # brief pause to allow processes to close
+} while (Get-Process chrome -ErrorAction SilentlyContinue)  # loop to handle if user have open many profile chrome window
+# Get-Process "chrome"  | Stop-Process # this forcefully to shutdown all process of Chrome as behavior of crash, leading to data lost
 $chromeArgs = "--restore-last-session", "--kiosk-printing", "--disable-popup-blocking", "--use-system-default-print"
 Start-Process "chrome" -ArgumentList $chromeArgs
