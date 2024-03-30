3caa655d14ff00659618931037a9bcfdb17bd18c
DuyDuc94 <duyduc.luonghuu@gmail.com>
2024-03-30 17:38:11 +0700

    chore: update issue comment

--- ForceRestartChrome.ps1
+++ ForceRestartChrome.ps1
@@ -1,3 +1,4 @@
+#issue: https://www.reddit.com/r/PowerShell/comments/186z5us/help_stopprocess_isnt_terminating_all_chrome/
 do {
     Get-Process chrome -ErrorAction SilentlyContinue | ForEach-Object { $_.CloseMainWindow() | Out-Null } # this shutdow only current selected instances of Chrome with behavior same as user click on Close button
     # Start-Sleep -Milliseconds 100  # brief pause to allow processes to close
